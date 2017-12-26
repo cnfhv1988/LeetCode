c1efc1cbf7ad67bdbe9f631c119bde2b65d384aa
Ma Chao <cnfhv1988@sina.com>
2017-12-26 21:32:54 +0800

    Create Longest Substring Without Repeating Characters.js

---
+++ 3.Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    var arr=[],max=0,i,len,index;
+    for(i=0,len=s.length;i<len;i++){
+        index = arr.indexOf(s[i]);
+        if(index >= 0){
+            max = Math.max(max,arr.length);
+            arr.splice(0,index+1);
+        }
+        arr.push(s[i]);  
+    }
+    return Math.max(max,arr.length);
+};
