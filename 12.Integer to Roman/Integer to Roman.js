acb6deac612534dcbe6591186459414337825f20
Ma Chao <cnfhv1988@sina.com>
2018-01-01 15:03:12 +0800

    Update Integer to Roman.js

--- 12.Integer to Roman/Integer to Roman.js
+++ 12.Integer to Roman/Integer to Roman.js
@@ -3,35 +3,26 @@
  * @return {string}
  */
 var intToRoman = function(num) {
-    var roman = ['M','D','C','L','X','V','I'],div=1000,i=0,result='';
+    var roman = ['M','D','C','L','X','V','I'],div=1000,i=0,result='',temp;
     while(num != 0){
-        switch(Math.floor(num/div)){
+        temp = Math.floor(num/div);
+        switch(temp){
             case 9:
                 result = result + roman[i] + roman[i-2];
                 break;
             case 8:
-                result = result + roman[i-1] + roman[i] + roman[i] + roman[i];
-                break;
             case 7:
-                result = result + roman[i-1] + roman[i] + roman[i];
-                break;
             case 6:
-                result = result + roman[i-1] + roman[i];
-                break;
             case 5:
-                result = result + roman[i-1];
+                result = result + roman[i-1] + (new Array(temp-4)).join(roman[i]);
                 break;
             case 4:
                 result = result + roman[i] + roman[i-1];
                 break;
             case 3:
-                result = result + roman[i] + roman[i] + roman[i];
-                break;
             case 2:
-                result = result + roman[i] + roman[i];
-                break;
             case 1:
-                result = result + roman[i];
+                result = result + (new Array(temp+1)).join(roman[i]);
                 break;
             case 0:
                 break;
