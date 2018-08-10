d40f309b95081c80b4997ad6a48667b72e6aea92
Ma Chao <cnfhv1988@sina.com>
2018-08-10 15:38:34 +0800

    Create String to Integer.js

---
+++ 008.String to Integer/String to Integer.js
@@ -0,0 +1,23 @@
+var myAtoi = function(str) {
+
+    var MAX = Math.pow(2,31) - 1;
+    var MIN = Math.pow(2,31) * (-1);
+    var s = str.trim();
+    var positive = true;
+    if(s.length === 0) return 0;
+    if(s[0] === '+' || s[0] === '-'){
+        positive = s[0] === '-' ? false : true;
+        s = s.slice(1)
+    }
+
+    var i = 0;
+    var result = 0;
+    while(i < s.length && s[i].charCodeAt() >= 48 && s[i].charCodeAt() <= 57){
+        result = result *10 + s[i].charCodeAt() - 48;
+        i++;
+    }
+    result = positive ? result : result * (-1);
+    result = result > MAX ? MAX : result;
+    result = result < MIN ? MIN : result;
+    return result;
+};
