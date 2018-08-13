281bb0aacb91bc50e47c8f936a590e2d871e53f8
Ma Chao <cnfhv1988@sina.com>
2018-08-13 16:09:33 +0800

    Update Two Sum.js

--- 001.Two Sum/Two Sum.js
+++ 001.Two Sum/Two Sum.js
@@ -3,6 +3,7 @@
  * @param {number} target
  * @return {number[]}
  */
+//29 test cases passed, beats 31.90% submissions.
 var twoSum = function(nums, target) {
     for(var i=0;i<nums.length;i++){
         if(nums.indexOf(target-nums[i],i+1) >= 0){
