ad8fc5310b79e21446be27d9fb57f280ac21b362
Ma Chao <cnfhv1988@sina.com>
2018-08-14 14:57:55 +0800

    Update Add Two Number.js

--- 002.Add Two Numbers/Add Two Number.js
+++ 002.Add Two Numbers/Add Two Number.js
@@ -10,33 +10,33 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+//beats 98.54% Javascript submissions
 var addTwoNumbers = function(l1, l2) {
-    if( !l1 && !l2 )return null;
-    var start = new ListNode(0);
-    var cursor = start;
-    while( l1 || l2 ){
-        if( l1 && l2 ){
-            cursor.next = new ListNode( l1.val + l2.val );
-        }
-        else{
-            cursor.next = l1 ? l1 : l2;
-        }
-        l1 = l1 ? l1.next : null;
-        l2 = l2 ? l2.next : null;
-        cursor = cursor.next;
-    };
-    cursor = start.next;
-    while(cursor){
-        if( cursor.val > 9){
-            cursor.val %= 10;
-            if(cursor.next){
-                cursor.next.val++;
-            }
-            else{
-                cursor.next = new ListNode(1);
-            }
-        }
-        cursor = cursor.next;
-    };
-    return start.next;
+
+    if(!l1 && !l2) return null;
+    var flag = 0;
+    var start = new ListNode(0);
+    var cursor = start;
+    while(l1 && l2) {
+        cursor.next = new ListNode((l1.val + l2.val + flag)%10);
+        flag = l1.val + l2.val + flag > 9 ? 1 : 0;
+        l1 = l1.next;
+        l2 = l2.next;
+        cursor = cursor.next;
+    }
+    cursor.next = l1 ? l1 : l2;
+    while(cursor.next && flag === 1){
+        cursor.next.val++;
+        if(cursor.next.val > 9) {
+            cursor.next.val %= 10;
+        }
+        else{
+            flag = 0;
+        }
+        cursor = cursor.next;
+    }
+    if(flag) {
+        cursor.next = new ListNode(1);
+    }
+    return start.next;
 };
