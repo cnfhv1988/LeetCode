7c6fd4ffbd67cc6cdabb5819f283125d60d79a2e
Ma Chao <cnfhv1988@sina.com>
2018-08-14 15:15:00 +0800

    Update Reverse Integer.js

--- 007.Reverse Integer/Reverse Integer.js
+++ 007.Reverse Integer/Reverse Integer.js
@@ -2,7 +2,16 @@
  * @param {number} x
  * @return {number}
  */
+//beats97.39% submissions
 var reverse = function(x) {
-    var r = parseInt(Math.abs(x).toString().split("").reverse().join(""));
-    return (r >= Math.pow(2,31) || r < -Math.pow(2,31)) ? 0 : (x>=0 ? r : -r);
+    var flag = !!(x >> 31);
+    var result = 0, MAX = Math.pow(2,31) - 1, MIN = -Math.pow(2, 31)
+    x = Math.abs(x);
+    while(x > 0){
+        result = result * 10 + x % 10;
+        x = Math.floor(x/10);
+    }
+    result = flag ? result * (-1) : result;
+    if(result > MAX || result < MIN) return 0;
+    return result;
 };
