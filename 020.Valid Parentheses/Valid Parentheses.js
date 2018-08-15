5639119e3aff4c15c0bc4da9711c7e01c46e343c
Ma Chao <cnfhv1988@sina.com>
2018-08-15 14:00:18 +0800

    Update Valid Parentheses.js

--- 020.Valid Parentheses/Valid Parentheses.js
+++ 020.Valid Parentheses/Valid Parentheses.js
@@ -2,22 +2,20 @@
  * @param {string} s
  * @return {boolean}
  */
- // 73 test cases passed, beats 7.24% of Javascript submissions
+ // 73 test cases passed, beats 80.12% of Javascript submissions
 var isValid = function(s) {
-
-    var parenthese = {')':'(',']':'[','}':'{'},stack=[];
-    for(var i=0;i<s.length;i++){
-        if(s[i] == '{' || s[i] == '[' || s[i] == '('){
+    var stack = [], pairs = {')':'(', ']':'[', '}':'{'};
+    for(var i = 0; i < s.length; i++) {
+        if(s[i] === '(' || s[i] === '[' || s[i] === '{') {
             stack.push(s[i]);
         }
         else{
-            if(stack.length == 0 || stack.pop() != parenthese[s[i]] ){
+            var c = stack.pop();
+            if(!pairs.hasOwnProperty(s[i]) || pairs[s[i]] !== c){
                 return false;
             }
         }
     }
-    if(stack.length != 0){
-        return false;
-    }
+    if(stack.length > 0) return false;
     return true;
 };
